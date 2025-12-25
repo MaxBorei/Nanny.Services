e1c90a9fe0ac721a5c4c74e1e8cca80dd21fd7fd
maxborey <maxborey@gmail.com>
2025-12-25 17:20:44 +0200

    hero+heder+fiks

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Header from "../Header/Header";
 import Container from "../Container/Container";
 
 export default function Layout() {
   return (
     <>
       <Container>
-        <Header />
         <Outlet />
       </Container>
     </>
